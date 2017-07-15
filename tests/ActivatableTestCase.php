4e1c97e5707c49ef2b37009a7860cbc82b01a4a7
niclasleonbock <niclasleonbock@gmail.com>
2017-07-15 22:44:39 +0200

    :sparkles: Upgrade to laravel 5.4

--- tests/ActivatableTestCase.php
+++ tests/ActivatableTestCase.php
@@ -10,18 +10,26 @@ class ActivatableTestCase extends TestCase
     public function setUp()
     {
         parent::setUp();
-
-        $artisan = $this->app->make('artisan');
-
-        $artisan->call('migrate', [
+        /*$this->artisan('migrate', [
             '--database'    => 'testing',
             '--path'        => '../tests/database/migrations',
+        ]);*/
+
+        $this->loadMigrationsFrom([
+            '--database' => 'testing',
+            '--realpath' => realpath(__DIR__.'/database/migrations'),
         ]);
 
-        $artisan->call('db:seed', [
+        $topic = new Topic([
+            'title'     => 'Lorem ipsum dolor sit amet',
+        ]);
+
+        $topic->save();
+
+        /*$this->artisan('db:seed', [
             '--database'    => 'testing',
             '--class'       => 'TopicTableSeeder',
-        ]);
+        ]);*/
     }
 
     /**
@@ -32,14 +40,21 @@ class ActivatableTestCase extends TestCase
      */
     protected function getEnvironmentSetUp($app)
     {
+        parent::getEnvironmentSetUp($app);
+
         $app['path.base'] = __DIR__ . '/../src';
 
         $app['config']->set('database.default', 'testing');
-        $app['config']->set('database.connections.testing', array(
+        $app['config']->set('database.connections.testing', [
             'driver'    => 'sqlite',
             'database'  => ':memory:',
             'prefix'    => '',
-        ));
+        ]);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return ['Orchestra\Database\ConsoleServiceProvider'];
     }
 
     protected function getModel()
@@ -63,7 +78,6 @@ class ActivatableTestCase extends TestCase
         return $topic;
     }
 
-
     /**
      * Test only activated (default) functionality.
      *
@@ -77,11 +91,11 @@ class ActivatableTestCase extends TestCase
     }
 
     /**
-     * Test only activated (default) functionality.
+     * Test loading with deactivated included functionality.
      *
      * @test
      */
-    public function testWithDectivated()
+    public function testWithDeactivated()
     {
         $count = $this->getModel()->withDeactivated()->count();
 
@@ -99,7 +113,7 @@ class ActivatableTestCase extends TestCase
 
         $topic->activate();
 
-        $this->assertEquals(Carbon::now(), $topic->activated_at);
+        $this->assertEquals(Carbon::now()->toDateTimeString(), $topic->activated_at->toDateTimeString());
     }
 
     /**
