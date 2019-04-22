f6a3fca9065540581e546c37e4ed59cf94293cc7
babalubas090 <rick@interword.com.br>
2019-04-22 16:22:59 -0300

    Methods can now be called statically.
    * withDeactivated and onlyDeactivated moved to scope
    * Updated method declaration in scope file

--- src/ActivatableScope.php
+++ src/ActivatableScope.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace niclasleonbock\Eloquent;
 
 use Illuminate\Database\Eloquent\Builder;
@@ -7,6 +8,14 @@ use Illuminate\Database\Eloquent\Scope;
 
 class ActivatableScope implements Scope
 {
+
+    /**
+     * All of the extensions to be added to the builder.
+     *
+     * @var array
+     */
+    protected $extensions = ['WithDeactivated', 'WithoutDeactivated', 'OnlyDeactivated', 'Deactivate', 'Activate'];
+
     /**
      * Apply the scope to a given Eloquent query builder.
      *
@@ -16,23 +25,73 @@ class ActivatableScope implements Scope
     public function apply(Builder $builder, Model $model)
     {
         $builder->whereNotNull($model->getQualifiedActivatedAtColumn());
+    }
 
-        $builder->macro('withDeactivated', function (Builder $builder) {
-            $this->remove($builder);
+    public function extend(Builder $builder)
+    {
+        foreach ( $this->extensions as $extension ) {
+            $this->{"add{$extension}"}($builder);
+        }
+    }
+
+    public function addWithDeactivated(Builder $builder)
+    {
+        $builder->macro('withDeactivated', function (Builder $builder, $withDeactivated = true) {
+            if ( !$withDeactivated ) {
+                return $builder->withoutDeactivated();
+            }
+
+            return $builder->withoutGlobalScope($this);
+        });
+    }
+
+    public function addWithoutDeactivated(Builder $builder)
+    {
+        $builder->macro('withoutDeactivated', function (Builder $builder) {
+            $model = $builder->getModel();
+
+            $builder->withoutGlobalScope($this)->whereNotNull(
+                $model->getQualifiedActivatedAtColumn()
+            );
 
             return $builder;
         });
+    }
 
+    public function addOnlyDeactivated(Builder $builder)
+    {
         $builder->macro('onlyDeactivated', function (Builder $builder) {
             $model = $builder->getModel();
 
-            $this->remove($builder);
-
-            $builder
-                ->getQuery()
-                ->whereNotNull($model->getQualifiedActivatedAtColumn());
+            $builder->withoutGlobalScope($this)->whereNull(
+                $model->getQualifiedActivatedAtColumn()
+            );
 
             return $builder;
+        });
+    }
+
+    public function addDeactivate(Builder $builder)
+    {
+        $builder->macro('deactivate', function (Builder $builder) {
+            $model = $builder->getModel();
+
+            $builder->withDeactivated();
+
+            return $builder->update([$model->getActivatedAtColumn() => null]);
+        });
+    }
+
+    public function addActivate(Builder $builder)
+    {
+        $builder->macro('activate', function (Builder $builder) {
+            $model = $this->getModel();
+
+            $query = $model->newQuery()->where($model->getKeyName(), $model->getKey());
+
+            $model->{$model->getActivatedAtColumn()} = $time = $model->freshTimestamp();
+
+            $query->update([$model->getActivatedAtColumn() => $model->fromDateTime($time)]);
         });
     }
 
